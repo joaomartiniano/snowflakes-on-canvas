3ad2ac9fffc6e4f7a3d50b69d3471197f641da84
João Martiniano <44736477+joaomartiniano@users.noreply.github.com>
2022-08-20 13:07:19 +0100

    Add snowflake class

---
+++ js/snowflake.js
@@ -0,0 +1,50 @@
+class Snowflake {
+    constructor(x, y, radius, color, speed, ctx) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.speed = speed;
+        this.ctx = ctx;
+
+        this.opacity = Math.random();
+
+        if (this.opacity == 0)
+        {
+            this.opacity = 0.2;
+        }
+
+        this.color = `rgba(255, 255, 255, ${this.opacity})`;
+
+        this.wait = Math.floor(Math.random() * 3);
+        this.countdown = this.wait;
+    }
+
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI, false);
+        ctx.fill();
+    }
+
+    update(canvas) {
+        if (this.y < (canvas.height + this.radius))
+        {
+            if (this.countdown == 0)
+            {
+                this.y += this.speed;
+                this.countdown = this.wait;
+            }
+            else
+            {
+                --this.countdown;
+            }            
+        }
+        else
+        {
+            this.x = Math.floor(Math.random() * canvas.width);
+            this.y = Math.floor(Math.random() * (-canvas.height));
+            this.countdown = this.wait;
+        }
+    }
+}
